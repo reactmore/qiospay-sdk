0b3987e1817badf8d41bc8b7cd62ee7173e8f14a
reactmore <reactmorecom@gmail.com>
2026-03-02 19:38:29 +0700

    update parse

--- tests/ParseTest/ParseBankTest.php
+++ tests/ParseTest/ParseBankTest.php
@@ -24,31 +24,6 @@ class ParseBankTest extends TestCase
         $this->assertSame('01/10/2025 10:54', $parsed['datetime']);
     }
 
-    public function testParseResponseProcessed(): void
-    {
-        $body = "R#INV-1772362352 TFBCA10 0953955315, Mohon tunggu transaksi sedang diproses. Saldo 18.848 @ 02\/03\/2026 01:52";
-
-        $mockStream = $this->createMock(\Psr\Http\Message\StreamInterface::class);
-        $mockStream->method('__toString')->willReturn($body);
-
-        $mockResponse = $this->createMock(\Psr\Http\Message\ResponseInterface::class);
-        $mockResponse->method('getBody')->willReturn($mockStream);
-
-        $mockAdapter = $this->createMock(\Reactmore\SupportAdapter\Adapter\AdapterInterface::class);
-        $mockAdapter->expects($this->once())
-            ->method('get')
-            ->willReturn($mockResponse);
-
-        $service  = new Transactions($mockAdapter, $this->config);
-        $response = $service->h2h(['product' => 'TFBCA10', 'dest' => '0953955315']);
-
-        $this->assertSame('INV-1772362352', $response['data']['trxid']);
-        $this->assertSame('PROCESS', $response['data']['transaction_status']);
-        $this->assertSame('0953955315', $response['data']['account']);
-        $this->assertSame('TFBCA10', $response['data']['product']);
-        $this->assertSame('18.848', $response['data']['saldo']);
-    }
-
     public function testParseTransferBCA(): void
     {
         $message = "T#1901056 R#trx_1759291955, Alhamdulillah, SUKSES. Bank BCA-Saldo BCA 10.000.0953955315. SN: 2025100117611514797454387969. Saldo 48060 - 11900 = 36.160 @01\/10\/2025 11:12\r\nqiospay.id";
